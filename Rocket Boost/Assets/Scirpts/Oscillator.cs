6d1b189ce5fea6716509bb74eab22d9e96e25230
eliebeaino <eliebeaino89@gmail.com>
2022-04-25 00:52:53 +0300

    upload game files

---
+++ Rocket Boost/Assets/Scirpts/Oscillator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;                      // speed of movement cycle
+
+    float movementFactor;
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon)  return;   // protects against dividing by 0
+        //set movement factor
+        float cycles = Time.time / period;                      // grows continually from 0
+        const float tau = Mathf.PI / 2f;                        // full circle circumference - ~6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);             // from -1 to 1
+        movementFactor = rawSinWave / 2f + 0.5f;                // from 0 to 1
+
+        // move the object
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Rocket Boost/Assets/Scirpts/RocketController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RocketController : MonoBehaviour
+{
+
+    Rigidbody myRigibody;
+    [SerializeField] float boostPower =10f;
+    [SerializeField] float rotationPower = 10f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] AudioClip successSFX;
+
+    [SerializeField] ParticleSystem mainEngineVFX;
+    [SerializeField] ParticleSystem deathVFX;
+    [SerializeField] ParticleSystem successVFX;
+
+    bool collisionDisabled = false;
+
+    AudioSource audioSource;
+    enum State { Alive,Dying, Transcending}
+    State state = State.Alive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigibody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (state == State.Alive)
+        {
+            Thrusting();
+            Rotation();
+        }
+
+        if (Debug.isDebugBuild)
+        {
+            if (Input.GetKeyDown(KeyCode.L)) LoadNextLvl();
+            if (Input.GetKeyDown(KeyCode.C)) collisionDisabled = !collisionDisabled;
+        }
+    }
+    
+    private void Rotation()
+    {
+        myRigibody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationPower * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationPower * Time.deltaTime);
+        }
+        myRigibody.freezeRotation = false;
+
+    }
+
+    private void Thrusting()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigibody.AddRelativeForce(Vector3.up * boostPower * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+            mainEngineVFX.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineVFX.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive || collisionDisabled) return;
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                state = State.Transcending;
+                audioSource.Stop();
+                audioSource.PlayOneShot(successSFX);
+                successVFX.Play();
+                Invoke("LoadNextLvl", levelLoadDelay);
+                break;
+            default:
+                state = State.Dying;
+                audioSource.Stop();
+                audioSource.PlayOneShot(deathSFX);
+                Invoke("LoadFirstLvl", levelLoadDelay);
+                deathVFX.Play();
+                break;
+        }
+    }
+
+    private void LoadFirstLvl()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLvl()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
